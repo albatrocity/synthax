bb646cc8d2ab6441f938a484c0c2dc2c0b9f0a52
Ross Brown <ross@cremalab.com>
2015-06-22 19:17:26 -0500

    rename patchers to modulators, readme

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var midi     = require('midi');
 var power    = require('./lib/power');
 var Patchbay = require('./lib/patchbay');
+var SubMixer = require('./lib/submixer');
 var mappers  = require('./lib/mappers');
-var LFO      = require('./lib/patchers/LFO');
+var LFO      = require('./lib/modulators/LFO');
 
 power.on().then(function(channels) {
   console.log("Powered On");
 
   patchbay = new Patchbay(channels.input, channels.output);
-  patchbay.connect(new LFO(), mappers.lowpass);
-  patchbay.connect(new LFO(), mappers.reso);
 
-  channels.input.on('message', function(deltaTime, message, d) {
-    console.log('m:' + message + ' d:' + deltaTime);
-    channels.output.sendMessage([0x0B, 19, message]);
-  });
+  filter_mixer = new SubMixer();
+  filter_mixer.lineIn(new LFO(400));
+  filter_mixer.lineIn(new LFO(10));
+
+  patchbay.connect(filter_mixer, mappers.lowpass);
+  patchbay.connect(new LFO(200), mappers.reso);
+
+  // channels.input.on('signal', function(deltaTime, message, d) {
+  //   console.log('m:' + message + ' d:' + deltaTime);
+  //   channels.output.sendMessage([0x0B, 19, message]);
+  // });
 });
