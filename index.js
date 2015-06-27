0aba2a6354e4ba26dd3df2f4d4ad47104813020b
Ross Brown <ross@cremalab.com>
2015-06-27 15:50:36 -0500

    Specify node version that works with kinect, start modulator

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ var Patchbay = require('./lib/patchbay');
 var SubMixer = require('./lib/submixer');
 var mappers  = require('./lib/mappers');
 var LFO      = require('./lib/modulators/LFO');
+var Kinect   = require('./lib/modulators/kinect');
 
 power.on().then(function(channels) {
   console.log("Powered On");
 
   patchbay = new Patchbay(channels.input, channels.output);
 
-  filter_mixer = new SubMixer();
-  filter_mixer.lineIn(new LFO(400));
-  filter_mixer.lineIn(new LFO(10));
+  // filter_mixer = new SubMixer();
+  // filter_mixer.lineIn(new LFO(400));
+  // filter_mixer.lineIn(new LFO(10));
 
-  patchbay.connect(filter_mixer, mappers.lowpass);
-  patchbay.connect(new LFO(200), mappers.reso);
+  // patchbay.connect(filter_mixer, mappers.lowpass);
+  // patchbay.connect(new LFO(200), mappers.reso);
+  patchbay.connect(new Kinect(), mappers.lowpass);
 
   // channels.input.on('signal', function(deltaTime, message, d) {
   //   console.log('m:' + message + ' d:' + deltaTime);
