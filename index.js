384a3ee1b98c0f664ba39a1f50074d1dbee9648b
Ross Brown <ross@cremalab.com>
2015-06-28 13:46:15 -0500

    Adding kinect video input, color analyzer, and preamp

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-var midi     = require('midi');
-var power    = require('./lib/power');
-var Patchbay = require('./lib/patchbay');
-var SubMixer = require('./lib/submixer');
-var mappers  = require('./lib/mappers');
-var LFO      = require('./lib/modulators/LFO');
-var Kinect   = require('./lib/modulators/kinect');
+var midi        = require('midi');
+var power       = require('./lib/power');
+var Patchbay    = require('./lib/patchbay');
+var SubMixer    = require('./lib/submixer');
+var mappers     = require('./lib/mappers');
+var LFO         = require('./lib/input_sources/LFO');
+var KinectVideo = require('./lib/input_sources/kinect_video');
+var Preamp      = require('./lib/preamp');
+var Color       = require('./lib/analyzers/color_analyzer');
 
 power.on().then(function(channels) {
   console.log("Powered On");
 
-  patchbay = new Patchbay(channels.input, channels.output);
+  var patchbay = new Patchbay(channels.input, channels.output);
+  mappers.connect(channels.output);
 
-  // filter_mixer = new SubMixer();
+  filter_mixer = new SubMixer();
   // filter_mixer.lineIn(new LFO(400));
-  // filter_mixer.lineIn(new LFO(10));
-
-  // patchbay.connect(filter_mixer, mappers.lowpass);
+  filter_mixer.lineIn(new LFO(10));
   // patchbay.connect(new LFO(200), mappers.reso);
-  patchbay.connect(new Kinect(), mappers.lowpass);
+  var video = new KinectVideo();
+  var ca = new Color;
 
-  // channels.input.on('signal', function(deltaTime, message, d) {
-  //   console.log('m:' + message + ' d:' + deltaTime);
-  //   channels.output.sendMessage([0x0B, 19, message]);
-  // });
+  green_pre = new Preamp({
+    dataValue: 'green', rmin: 0, rmax: 256
+  });
+  
+  patchbay.connect(video, ca);
+  patchbay.connect(ca, green_pre);
+
+  filter_mixer.lineIn(video_pre);
+  patchbay.connect(filter_mixer, mappers.lowpass);
+
 });
