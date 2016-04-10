09ff49ab1a082121b8bbb81bbee51985641106eb
Ross Brown <ross@cremalab.com>
2016-04-10 17:39:16 -0500

    library directories

--- index.js
+++ index.js
@@ -1,53 +1,9 @@
-var midi        = require('midi');
-var power       = require('./lib/power');
-var Patchbay    = require('./lib/patchbay');
-var SubMixer    = require('./lib/submixer');
-var Buss        = require('./lib/buss');
-var ActionResponder = require('./lib/action_responder');
-var phatty      = require('./lib/mappers/sub_phatty');
-var LFO         = require('./lib/input_sources/LFO');
-var KinectVideo = require('./lib/input_sources/kinect_video');
-var Preamp      = require('./lib/preamp');
-var Color       = require('./lib/analyzers/color_analyzer');
-var Faces       = require('./lib/analyzers/face_detector');
-var cv          = require('opencv');
-var CameraVideo = require('./lib/input_sources/camera');
-
-
-power.on().then(function(channels) {
-  console.log("Powered On");
-
-  var patchbay = new Patchbay(channels.input, channels.output);
-  phatty.connect(channels.output);
-
-  var video = new KinectVideo();
-  var videoBuss = new Buss;
-  videoBuss.lineIn(video);
-
-  var ca = new Color;
-  green_pre = new Preamp({
-    dataValue: 'green', rMin: 0, rMax: 256
-  });
-  patchbay.connect(videoBuss, ca);
-  patchbay.connect(ca, green_pre);
-  patchbay.connect(green_pre, phatty.lowpass);
-
-  var faces = new Faces;
-  var eye_contact = new ActionResponder({
-    responders: [
-      {
-        action: 'eyes_closed',
-        mapper: phatty.note_off,
-        value: 100
-      },
-      {
-        action: 'eyes_opened',
-        mapper: phatty.note_on,
-        value: 100
-      }
-    ]
-  });
-  patchbay.connect(videoBuss, faces);
-  patchbay.connect(faces, eye_contact);
-
-});
+exports.Patchbay        = require('./lib/patchbay');
+exports.power           = require('./lib/power');
+exports.SubMixer        = require('./lib/submixer');
+exports.Buss            = require('./lib/buss');
+exports.ActionResponder = require('./lib/action_responder');
+exports.Preamp          = require('./lib/preamp');
+exports.InputSources    = require('./lib/input_sources');
+exports.Analyzers       = require('./lib/analyzers');
+exports.Mappers         = require('./lib/mappers');
